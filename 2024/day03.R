70342688b8489a091bd8286e78b5cebdee0f438a
schochastics <david@schochastics.net>
2024-12-03 12:17:45 +0100

    added a stringi solution

--- 2024/day03.R
+++ 2024/day03.R
@@ -1,10 +1,25 @@
 input <- paste0(readLines("input03.txt"), collapse = "")
-mul <- `*`
+## Parse/eval
 
-mul_str <- unlist(stringr::str_extract_all(input, "mul\\([0-9]+,[0-9]+\\)"))
-sum(vapply(mul_str, \(x) eval(parse(text = x)), numeric(1)))
+eval_parse <- function(input) {
+  mul <- `*`
+  mul_str <- unlist(stringr::str_extract_all(input, "mul\\([0-9]+,[0-9]+\\)"))
+  sum(vapply(mul_str, \(x) eval(parse(text = x)), numeric(1)))
+}
+eval_parse(input)
 
 input <- paste0("do()", input, "don't()")
 matches <- paste0(stringr::str_extract_all(input, "(?<=do\\(\\))(.*?)(?=don't\\(\\))")[[1]], collapse = "")
 mul_str <- unlist(stringr::str_extract_all(matches, "mul\\([0-9]+,[0-9]+\\)"))
 sum(vapply(mul_str, \(x) eval(parse(text = x)), numeric(1)))
+
+stringi_match <- function(input) {
+  regex <- "mul\\(([0-9]+),([0-9]+)\\)"
+  matches <- stringi::stri_match_all_regex(input, regex)[[1]]
+  sum(as.numeric(matches[, 2]) * as.numeric(matches[, 3]))
+}
+
+microbenchmark::microbenchmark(
+  eval_parse(input),
+  stringi_match(input)
+)
